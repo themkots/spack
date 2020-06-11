f5705a7fc49dd735eeb89058324841f5fc189fcb
Todd Gamblin <tgamblin@llnl.gov>
2020-06-10 20:45:16 -0700

    concretizer: use cardinality constraints for versions
    Instead of python callbacks, use cardinality constraints for package
    versions. This is slightly faster and has the advantage that it can be
    written to an ASP program to be executed *outside* of Spack. We can use
    this in the future to unify the pyclingo driver and the clingo text
    driver.
    
    This makes use of add_weight_rule() to implement cardinality constraints.
    add_weight_rule() only has a lower bound parameter, but you can implement
    a strict "exactly one of" constraint using it. In particular, wee want to
    define:
    
        1 {v1; v2; v3; ...} 1 :- version_satisfies(pkg, constraint).
        version_satisfies(pkg, constraint) :- 1 {v1; v2; v3; ...} 1.
    
    And we do that like this, for every version constraint:
    
        atleast1(pkg, constr) :- 1 {version(pkg, v1); version(pkg, v2); ...}.
        morethan1(pkg, constr) :- 2 {version(pkg, v1); version(pkg, v2); ...}.
        version_satisfies(pkg, constr) :- atleast1, not morethan1(pkg, constr).
        :- version_satisfies(pkg, constr), morethan1.
        :- version_satisfies(pkg, constr), not atleast1.
    
    v1, v2, v3, etc. are computed on the Python side by comparing every
    possible package version with the constraint.
    
    Computing things like this has the added advantage that if v1, v2, v3,
    etc. comprise *all* possible versions of a package, we can just omit the
    rules for the constraint under consideration. This happens pretty
    frequently in the Spack mainline.

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -2383,6 +2383,7 @@ class Spec(object):
 
         result = spack.solver.asp.solve([self])
         if not result.satisfiable:
+            result.print_cores()
             raise spack.error.UnsatisfiableSpecError(
                 self, "unknown", "Unsatisfiable!")
 
